2ff4402cd97e698ebd0eaca9057bbd550bcd15c7
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2024-05-07 22:53:47 +0800

    Changed path

--- addons/node_components/scripts/classes/node_component.gd
+++ addons/node_components/scripts/classes/node_component.gd
@@ -1,4 +1,4 @@
 @tool
-@icon("res://addons/macchi's_node_components/class_icons/node-component.png")
+@icon("res://addons/node_components/class_icons/node-component.png")
 extends Node
 class_name NodeComponent
